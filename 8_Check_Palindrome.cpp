b02badc8b77bd9b2773b64f584674ad74a671ee6
Azeem Khan <98760240+Azeemo@users.noreply.github.com>
2023-05-08 21:56:54 +0530

    Update 8_Check_Palindrome.cpp

--- 8_Check_Palindrome.cpp
+++ 8_Check_Palindrome.cpp
@@ -1,56 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-bool isPalRec(char str[],
-              int s, int e)
-{
-     
-    // If there is only one character
-    if (s == e)
-    return true;
- 
-    // If first and last
-    // characters do not match
-    if (str[s] != str[e])
-    return false;
- 
-    // If there are more than
-    // two characters, check if
-    // middle substring is also
-    // palindrome or not.
-    if (s < e + 1)
-    return isPalRec(str, s + 1, e - 1);
- 
-    return true;
+bool sentencePalindrome(string &s,int i){
+	if (i>=s.size()/2)
+	{
+		return true;
+	}
+	else{
+		if (s[i]!=s[s.size()-i-1])
+		{
+			return false;
+		}
+		sentencePalindrome(s,i+1);
+	}
 }
-/*
+
  // USING TWO POINTER
- 	bool sentencePalindrome(string s){
-    int i=0,j=s.size()-1;
-    //initializing both the pointers at beginnning and end
-    while(i<=j){
-        if(!(s[i]>= 'a' && s[i]<='z'))  //space at the beginnning 
-             i++;
-        else if(!(s[j]>= 'a' && s[j]<='z'))  // space at ending
-             j--;
-        else if(s[i]!=s[j])
-             return false;
-        else{
-            i++;
-            j--;
-        }
-    }
+ // 	bool sentencePalindrome(string s){
+ //    int i=0,j=s.size()-1;
+ //    //initializing both the pointers at beginnning and end
+ //    while(i<=j){
+ //        if(!(s[i]>= 'a' && s[i]<='z'))  //space at the beginnning 
+ //             i++;
+ //        else if(!(s[j]>= 'a' && s[j]<='z'))  // space at ending
+ //             j--;
+ //        else if(s[i]!=s[j])
+ //             return false;
+ //        else{
+ //            i++;
+ //            j--;
+ //        }
+ //    }
 
-    return true;
- }
-*/
+ //    return true;
+ // }
 
-
-
-int main()
+int main(){
+string s;
+getline(cin,s);
+if (sentencePalindrome(s,0))
 {
-char s[]  = "Azeem";
-isPalRec(s);
-
+       cout<<"Palindrome"<<endl;
+}
+else{
+	cout<<"Not Palindrome"<<endl;
+}
 return 0;
 
 }
